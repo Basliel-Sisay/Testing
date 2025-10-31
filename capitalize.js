bf3daeb2613efe8501a0eccd9942658ff7116467
Basliel2025 <basliel.ugr-3563-16@aau.edu.et>
2025-10-31 15:07:00 +0300

    Remove capitalize.js from root directory

--- capitalize.js
+++
@@ -1,8 +0,0 @@
-function capital(word){
-    //store the first word in a given word and capitalize the word
-    const firstWord = word.charAt(0).toUpperCase();
-    //store the rest of other words like if the word is Yeabsera, otherWords will store the words after the first word which is eabsera
-    const otherWords= word.slice(1);
-    // return the full word 
-    return firstWord + otherWords;
-}
